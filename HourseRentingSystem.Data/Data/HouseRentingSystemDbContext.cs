9aa5eadc1eed7b177ab05b7e1c7087f83b12268e
TomaNeshkovSoftUni <toma.neshkov.highschool@buditel.bg>
2026-03-24 11:16:40 +0200

    Added db context

---
+++ HourseRentingSystem.Data/Data/HouseRentingSystemDbContext.cs
@@ -0,0 +1,27 @@
+﻿using HouseRentingSystem.Data.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HouseRentingSystem.Data.Data
+{
+    public class HouseRentingSystemDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public HouseRentingSystemDbContext(DbContextOptions<HouseRentingSystemDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<House> Houses { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Agent> Agents { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
