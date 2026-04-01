ce9e0e0136c7b1ec9fbcb03fda69267d919e65ef
toman <toma.neshkov.highschool@buditel.bg>
2026-04-01 20:32:26 +0300

    Fixed seeding

--- HourseRentingSystem.Data/Configuration/AgentConfiguration.cs
+++ HourseRentingSystem.Data/Configuration/AgentConfiguration.cs
@@ -11,26 +11,26 @@ namespace HouseRentingSystem.Data.Configuration
     {
         public void Configure(EntityTypeBuilder<Agent> builder)
         {
-            // builder.HasData(SeedAgents());
+            builder.HasData(SeedAgents());
         }
 
         public IEnumerable<Agent> SeedAgents()
         {
             return new Agent[]
             {
-                new Agent
-                {
-                    Id = 1,
-                    PhoneNumber = "0888888888",
-                    UserId = "1"
-                },
+                //new Agent
+                //{
+                //    Id = 1,
+                //    PhoneNumber = "0888888888",
+                //    UserId = "1"
+                //},
 
-                new Agent
-                {
-                    Id = 2,
-                    PhoneNumber = "0812345678",
-                    UserId = "1"
-                }
+                //new Agent
+                //{
+                //    Id = 2,
+                //    PhoneNumber = "0812345678",
+                //    UserId = "1"
+                //}
 
             };
         }
--- HourseRentingSystem.Data/Configuration/CategoryConfiguration.cs
+++ HourseRentingSystem.Data/Configuration/CategoryConfiguration.cs
@@ -11,18 +11,18 @@ namespace HouseRentingSystem.Data.Configuration
     {
         public void Configure(EntityTypeBuilder<Category> builder)
         {
-            // builder.HasData(SeedCategories());
+            builder.HasData(SeedCategories());
         }
 
         private IEnumerable<Category> SeedCategories()
         {
             return new Category[]
             {
-                new Category { Id = 1, Name = "Cottage" },
-                new Category { Id = 2, Name = "Single-Family" },
-                new Category { Id = 3, Name = "Duplex" },
-                new Category { Id = 4, Name = "Townhouse" },
-                new Category { Id = 5, Name = "Apartment" }
+                //new Category { Id = 1, Name = "Cottage" },
+                //new Category { Id = 2, Name = "Single-Family" },
+                //new Category { Id = 3, Name = "Duplex" },
+                //new Category { Id = 4, Name = "Townhouse" },
+                //new Category { Id = 5, Name = "Apartment" }
             };
         }
     }
