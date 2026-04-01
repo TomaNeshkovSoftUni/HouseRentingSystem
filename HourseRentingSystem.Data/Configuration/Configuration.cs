df32b5940c4f97cd88abfc64eb2d96e932846279
toman <toma.neshkov.highschool@buditel.bg>
2026-04-01 20:37:01 +0300

    bugs

---
+++ HourseRentingSystem.Data/Configuration/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HouseRentingSystem.Data.Configuration
+{
+    public class Configuration
+    {
+        public static string ConnectionString = "";
+    }
+}
